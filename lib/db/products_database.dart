4a9b2dc7710c0bf9d19c85a8fd24da7f9b62d056
black <black@kali.Blacktaler>
2022-11-30 19:20:56 +0500

    update va delete methodlari o'zgardi

--- lib/db/products_database.dart
+++ lib/db/products_database.dart
@@ -1,5 +1,6 @@
 import 'package:dars26/models/product_fields.dart';
 import 'package:dars26/models/product_model.dart';
+
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -10,12 +11,16 @@ class ProductsDatabase {
 
   static Database? _database;
 
+//4
   Future<Database> get database async {
     if (_database != null) return _database!;
 
+
+  //3
     return await _initDB('products.db');
   }
 
+// 2
   _initDB(String filePath) async {
     final dbPath = await getDatabasesPath();
     final path = join(dbPath, filePath);
@@ -29,6 +34,8 @@ class ProductsDatabase {
     );
   }
 
+
+// 1
   _createDB(Database db, int version) async {
     await db.execute('''
     CREATE TABLE IF NOT EXISTS $productTable(
@@ -62,5 +69,20 @@ class ProductsDatabase {
     final data = await db.query(productTable, where: '_id', whereArgs: [id]);
 
     return ProductModel.fromMap(data.first);
+  }
+
+  update(ProductModel product,int id) async {
+    final db = await instance.database;
+
+    await db.update(productTable, product.toMap(),
+    where: '${ProductFields.id}==?', // _id == ? ya'ni "_id" == id  
+    whereArgs: [id] // id si orqali update qilinadi
+    );
+  }
+
+  delete(int id) async {
+    final db = await instance.database;
+
+    await db.delete(productTable, where: ProductFields.id, whereArgs: [id]);
   }
 }
