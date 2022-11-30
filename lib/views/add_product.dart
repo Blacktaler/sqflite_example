28e6f1e07d38c8610ff6d60d6c8f4ed41bfc70fd
black <black@kali.Blacktaler>
2022-11-30 19:18:23 +0500

    id qo'shildi

--- lib/views/add_product.dart
+++ lib/views/add_product.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:dars26/db/products_database.dart';
 import 'package:dars26/models/product_fields.dart';
 import 'package:dars26/models/product_model.dart';
@@ -6,8 +8,11 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class AddProduct extends StatefulWidget {
-  AddProduct(this.callback);
-  void Function(List<ProductModel> products) callback;
+  AddProduct(this.callback, {required this.isUpdate, required this.id});
+  int id; //id update methodi uchun qo'shildi
+  bool isUpdate;
+
+  void Function(List<ProductModel> products)? callback;
 
   @override
   State<AddProduct> createState() => _AddProductState();
@@ -19,7 +24,7 @@ class _AddProductState extends State<AddProduct> {
 
   @override
   void dispose() {
-    widget.callback(products!);
+    widget.callback!(products!);
 
     super.dispose();
   }
@@ -35,9 +40,28 @@ class _AddProductState extends State<AddProduct> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
+        title: Text(widget.isUpdate ? "Edit Product" : "Add Product"),
         actions: [
           TextButton(
-            onPressed: buttonFunction,
+            onPressed: () async {
+              final productDB = ProductsDatabase.instance;
+
+              final product = ProductModel(
+                  id: int.parse(controllers[0].text),
+                  name: controllers[1].text,
+                  companyName: controllers[2].text,
+                  price: int.parse(controllers[3].text));
+
+              widget.isUpdate
+                  ? productDB.update(product, widget.id) //id bu yerga beriladi
+                  : productDB.create(product);
+
+              products = await ProductsDatabase.instance.readAllProducts();
+              setState(() {});
+
+              Navigator.pop(context);
+            },
             child: Text("save"),
           ),
         ],
@@ -51,31 +75,16 @@ class _AddProductState extends State<AddProduct> {
           SizedBox(
             height: 100,
           ),
-          // ElevatedButton(onPressed: buttonFunction, child: Text("Enter")),
         ],
       ),
     );
   }
 
-  TextFormField textMethod(TextEditingController controller) {
+  textMethod(TextEditingController controller) {
     return TextFormField(
       controller: controller,
       decoration: InputDecoration(border: OutlineInputBorder()),
     );
   }
 
-  buttonFunction() async {
-    final product = ProductModel(
-        id: int.parse(controllers[0].text),
-        name: controllers[1].text,
-        companyName: controllers[2].text,
-        price: int.parse(controllers[3].text));
-
-    ProductsDatabase.instance.create(product);
-
-    products = await ProductsDatabase.instance.readAllProducts();
-    setState(() {});
-
-    Navigator.pop(context);
-  }
 }
